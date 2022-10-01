a4e205b81160c7a7c84f73423a1c1659b7e85bf4
tommytommy-hero <109708488+tommytommy-hero@users.noreply.github.com>
2022-10-01 23:26:39 +0000

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,36 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/index'
+    get 'users/show'
+    get 'users/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'recipes/show'
+    get 'recipes/index'
+    get 'recipes/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'recipes/new'
+    get 'recipes/index'
+    get 'recipes/show'
+    get 'recipes/edit'
+  end
+  namespace :public do
+    get 'users/show'
+    get 'users/index'
+    get 'users/edit'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   devise_for :users,skip:[:passwords], controllers:{
     registrations: "public/registrations",
     sessions: "public/sessions"
