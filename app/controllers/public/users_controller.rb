93c5bc954a38c986a930a3f0bf8f80d1ff02ccd1
tommytommy-hero <109708488+tommytommy-hero@users.noreply.github.com>
2022-10-04 04:21:43 +0000

    Fix:favorite

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,21 @@
 class Public::UsersController < ApplicationController
-  
-  
+
+
   def show
     @user = User.find(params[:id])
     @recipes = @user.recipes.order(created_at: "DESC")
     @favorite = 0
-    @recipes.each do |recipe|
-      @favorite += recipe.favorites.size
-    end
-    
-    
-    
+    #いいね総数
+    #@fav_recipes = current_user.recipes
+    #@fav_recipes.each do |recipe|
+     # @favorite += recipe.favorites.count
+    #end
+  end
+
+  def favorites
+    @user = User.find(params[:id])
+    favorites = Favorite.where(user_id: @user.id).pluck(:recipe_id)
+    @recipes = Recipe.find(favorites)
   end
 
   def index
