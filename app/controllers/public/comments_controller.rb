7b0a66f12e2a3ba16e897a43ecce90ee390c4ef8
tommytommy-hero <109708488+tommytommy-hero@users.noreply.github.com>
2022-10-04 01:15:41 +0000

    Add:comment(非同期)

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,21 @@
 class Public::CommentsController < ApplicationController
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.recipe_id = @recipe.id
+    @comment.save
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:recipe_comment)
+  end
 end
--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -22,6 +22,7 @@ class Public::RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @comment = Comment.new
   end
 
   def confirm
